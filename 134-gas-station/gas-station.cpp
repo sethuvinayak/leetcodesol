14c2bad960f4708f9da6a89ee78a578367f5c49e
Sethu Vinayak <146705991+sethuvinayak@users.noreply.github.com>
2025-09-11 15:20:38 +0530

    Time: 0 ms (100.00%) | Memory: 112.4 MB (44.48%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total = 0;     // total gas - total cost
-        int tank = 0;      // current fuel in tank
-        int start = 0;     // candidate start index
+        int total = 0;     
+        int tank = 0;      
+        int start = 0;     
 
         for (int i = 0; i < gas.size(); i++) {
             total += gas[i] - cost[i];
             tank  += gas[i] - cost[i];
 
             if (tank < 0) {
-                // can't reach station i+1, so restart from i+1
+                
                 start = i + 1;
                 tank = 0;
             }
