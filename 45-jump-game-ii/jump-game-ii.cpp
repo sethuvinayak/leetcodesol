e7b7e63ed79ba834b4c1f88034beaf06246ae3ef
Sethu Vinayak <146705991+sethuvinayak@users.noreply.github.com>
2025-09-12 01:17:21 +0530

    Time: 3 ms (32.21%) | Memory: 20.6 MB (37.14%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int jumps=0,far=0,end=0;
+        for(int i=0;i<n-1;i++){
+            far=max(far,i+nums[i]);
+            if(i==end){
+                jumps++;
+                end=far;
+            }
+        }
+        return jumps;
+        
+    }
+};
