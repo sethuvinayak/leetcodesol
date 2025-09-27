f252654375a35a6c98dc46191a7b74573fd48c77
Sethu Vinayak <146705991+sethuvinayak@users.noreply.github.com>
2025-09-27 12:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (83.51%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,11 +4,11 @@ public:
         int n = nums.size();
         k = k % n; // normalize k
 
-        // Reverse entire array
+        
         reverse(nums.begin(), nums.end());
-        // Reverse first k elements
+        
         reverse(nums.begin(), nums.begin() + k);
-        // Reverse remaining n-k elements
+        
         reverse(nums.begin() + k, nums.end());
     }
 };
